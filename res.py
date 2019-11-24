ecf89bf566b413dac396d06786fa8230e4f8c3c5
heinwol <heinwol@users.noreply.github.com>
2019-11-24 14:55:50 +0300

    добавлен res.py

---
+++ res.py
@@ -0,0 +1,47 @@
+import sympy as sp
+import numpy as np
+import matplotlib
+from sympy.interactive import printing
+from sympy.parsing.sympy_parser import parse_expr
+from sympy.printing.latex import latex
+import typing
+from IPython.display import Latex
+
+#printing.init_printing(use_latex=True)
+#sp.init_session()
+
+xpr = parse_expr
+
+
+class Eqn(sp.Eq):
+    def __new__(cls, *args, **kwargs):
+        instance = super(Eqn, cls).__new__(cls, *args, **kwargs)
+        return instance
+
+    def __mul__(self, expr):
+        left = self.lhs * expr
+        right = self.rhs * expr
+        return Eqn(left, right)
+
+    def __add__(self, expr):
+        left = self.lhs + expr
+        right = self.rhs + expr
+        return Eqn(left, right)
+
+    def __sub__(self, expr):
+        left = self.lhs - expr
+        right = self.rhs - expr
+        return Eqn(left, right)
+
+    def __truediv__(self, expr):
+        left = self.lhs / expr
+        right = self.rhs / expr
+        return Eqn(left, right)
+
+    def __pow__(self, expr):
+        left = self.lhs ** expr
+        right = self.rhs ** expr
+        return Eqn(left, right)
+
+def flatex(text: str, *args):
+    return Latex(text.format(sp.latex(args, mode='plain')))
