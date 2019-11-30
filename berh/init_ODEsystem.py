ae2c80c147a8b4e114ee00c9a5d223d018440adc
Joe-Berh <podderegino@gmail.com>
2019-11-30 21:49:20 +0300

    Впервые отправляю коммит из VSCode ...

--- berh/init_ODEsystem.py
+++ berh/init_ODEsystem.py
@@ -10,6 +10,7 @@ x = sp.symarray('x', 5)
 #C, P, R, T == x[1], x[2], x[3], x[4] respectively
 
 xpr = parse_expr
+some_var = 606
 
 #equation parameters
 k_c, mu_c,     C_0,     lambda_c, K_c      = sp.symbols("k_c, mu_c, C_0, lambda_c, K_c")
@@ -26,17 +27,17 @@ params['gamma_p'] = 0.02 * params['lambda_r'] / (params['P_0'] * (1 - params['la
 params['gamma_c'] = params['gamma_p']
 params['K_t'] = params['K_c']
 
-f1 = (k_c + mu_c*x[2]) * x[1]**(3/4) * (1 - (x[1]/C_0)**(1/4) ) - \
-        (lambda_c * x[1]*x[4]) / (K_c + 1 - x[3])
-f2 = x[2] * (1 - x[2] / P_0) * (k_p + mu_p*x[1] / (K_p + x[1]) ) - lambda_p*x[2]
-f3 = k_r - x[3] * (lambda_r + gamma_p*x[2] + gamma_c*x[1])
-f4 = k_t*x[3] / (K_t - x[3] + 1) - lambda_t*x[4]
-
-# f1 = (k_c + mu_c*x[2]) * x[1]**xpr("3/4") * (1 - (x[1]/C_0)**xpr("1/4") ) - \
+# f1 = (k_c + mu_c*x[2]) * x[1]**(3/4) * (1 - (x[1]/C_0)**(1/4) ) - \
 #         (lambda_c * x[1]*x[4]) / (K_c + 1 - x[3])
 # f2 = x[2] * (1 - x[2] / P_0) * (k_p + mu_p*x[1] / (K_p + x[1]) ) - lambda_p*x[2]
 # f3 = k_r - x[3] * (lambda_r + gamma_p*x[2] + gamma_c*x[1])
 # f4 = k_t*x[3] / (K_t - x[3] + 1) - lambda_t*x[4]
+
+f1 = (k_c + mu_c*x[2]) * x[1]**xpr("3/4") * (1 - (x[1]/C_0)**xpr("1/4") ) - \
+        (lambda_c * x[1]*x[4]) / (K_c + 1 - x[3])
+f2 = x[2] * (1 - x[2] / P_0) * (k_p + mu_p*x[1] / (K_p + x[1]) ) - lambda_p*x[2]
+f3 = k_r - x[3] * (lambda_r + gamma_p*x[2] + gamma_c*x[1])
+f4 = k_t*x[3] / (K_t - x[3] + 1) - lambda_t*x[4]
 
 F = sp.Matrix([f1, f2, f3, f4])
 
